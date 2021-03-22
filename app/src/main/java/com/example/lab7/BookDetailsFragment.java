374c92b1699dd9ea33ebe6264d2494772c37961b
gsobrinski <gsobrinski99@gmail.com>
2021-03-22 12:17:07 -0400

    fixed book details layout

--- app/src/main/java/com/example/lab7/BookDetailsFragment.java
+++ app/src/main/java/com/example/lab7/BookDetailsFragment.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -18,8 +19,7 @@ public class BookDetailsFragment extends Fragment {
 
     String bookTitle = "";
     public static final String BOOK_TITLE = "book_title";
-
-    ImageView imageView;
+    TextView textView;
 
     public BookDetailsFragment() {
         // Required empty public constructor
@@ -46,6 +46,11 @@ public class BookDetailsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_book_details, container, false);
+        View view = inflater.inflate(R.layout.fragment_book_details, container, false);
+
+        textView = view.findViewById(R.id.textView);
+
+
+        return view;
     }
 }
