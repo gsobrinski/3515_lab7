620f92c1f331d967487045aab8ba4ddf0cf5df53
gsobrinski <gsobrinski99@gmail.com>
2021-03-31 22:44:29 -0400

    added book search dialog and methods in mainactivity

--- app/src/main/java/com/example/lab7/BookSearchActivity.java
+++ app/src/main/java/com/example/lab7/BookSearchActivity.java
@@ -1,14 +1,56 @@
 package com.example.lab7;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 
-public class BookSearchActivity extends AppCompatActivity {
+public class BookSearchActivity extends DialogFragment {
+    // interface
+    SearchListenerInterface listener;
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        // set the layout for the popup search dialog
+        LayoutInflater inflater = requireActivity().getLayoutInflater();
+        builder.setView(inflater.inflate(R.layout.activity_book_search, null))
+        // create search button
+        .setPositiveButton(R.string.go, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+                // do search here
+            }
+        })
+                // create cancel button
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // cancel the search - go back to previous fragment
+                    }
+                });
+        return builder.create();
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_book_search);
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        // make sure the parent implements the searchListener interface
+        try {
+            // create the searchListener
+            listener = (SearchListenerInterface) context;
+        } catch (ClassCastException e) {}
     }
+
+    // parent must implement this interface to know what is happening in the dialog
+    public interface SearchListenerInterface {
+        public void onSearch(DialogFragment dialog);
+        public void onCancel(DialogFragment dialog);
+    }
+
 }
--- app/src/main/java/com/example/lab7/MainActivity.java
+++ app/src/main/java/com/example/lab7/MainActivity.java
@@ -2,11 +2,14 @@ package com.example.lab7;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity implements BookListFragment.BookListInterface {
+public class MainActivity extends AppCompatActivity implements BookListFragment.BookListInterface, BookSearchActivity.SearchListenerInterface {
 
     BookDetailsFragment bdFragment;
     BookListFragment blFragment;
@@ -16,13 +19,17 @@ public class MainActivity extends AppCompatActivity implements BookListFragment.
 
     public static final String SAVED_TITLE = "saved_title";
     public static final String SAVED_AUTHOR = "saved_author";
+    public static final String SAVED_COVER_URL = "saved_cover_url";
     String title;
     String author;
+    String coverURL;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.book_layout);
+
+        Button searchButton = (Button) findViewById(R.id.button);
 
         landscape = findViewById(R.id.frame2) != null;
 
@@ -33,22 +40,23 @@ public class MainActivity extends AppCompatActivity implements BookListFragment.
             fragmentManager.beginTransaction().replace(R.id.frame1, blFragment).addToBackStack(null).commit();
 
             if (landscape) {
-                bdFragment = BookDetailsFragment.newInstance(null, null);
+                bdFragment = BookDetailsFragment.newInstance(null, null, null);
                 fragmentManager.beginTransaction().replace(R.id.frame2, bdFragment).addToBackStack(null).commit();
             }
         } else {
             // retrieve saved instance state
             title = savedInstanceState.getString(SAVED_TITLE);
             author = savedInstanceState.getString(SAVED_AUTHOR);
+            coverURL = savedInstanceState.getString(SAVED_COVER_URL);
             // check if title and author have been created
             if(title != null && author != null) {
                 // if landscape/tablet
                 if (landscape) {
                     // reuse bdFragment if possible
                     if (bdFragment == null) {
-                        bdFragment = BookDetailsFragment.newInstance(title, author);
+                        bdFragment = BookDetailsFragment.newInstance(title, author, coverURL);
                     } else {
-                        bdFragment.setBookDetails(title, author);
+                        bdFragment.setBookDetails(title, author, coverURL);
                     }
                     fragmentManager.beginTransaction().replace(R.id.frame2, bdFragment).addToBackStack(null).commit();
 
@@ -63,14 +71,23 @@ public class MainActivity extends AppCompatActivity implements BookListFragment.
                 } else {
                     // reuse bdFragment if possible
                     if(bdFragment == null) {
-                        bdFragment = BookDetailsFragment.newInstance(title, author);
+                        bdFragment = BookDetailsFragment.newInstance(title, author, coverURL);
                     } else {
-                        bdFragment.setBookDetails(title, author);
+                        bdFragment.setBookDetails(title, author, coverURL);
                     }
                     fragmentManager.beginTransaction().replace(R.id.frame1, bdFragment).addToBackStack(null).commit();
                 }
             }
         }
+
+        // trigger the search dialog
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment dialog = new BookSearchActivity();
+                dialog.show(getSupportFragmentManager(), "BookSearchActivity");
+            }
+        });
     }
 
     // implemented from BookListFragment's interface
@@ -79,16 +96,27 @@ public class MainActivity extends AppCompatActivity implements BookListFragment.
         this.title = title;
         this.author = author;
         if(!landscape) {
-            BookDetailsFragment bdFragment = BookDetailsFragment.newInstance(title, author);
+            BookDetailsFragment bdFragment = BookDetailsFragment.newInstance(title, author, coverURL);
             fragmentManager.beginTransaction().replace(R.id.frame1, bdFragment).addToBackStack(null).commit();
         } else {
             // check if bdfragment is null
             if(bdFragment == null) {
-                bdFragment = BookDetailsFragment.newInstance(title, author);
+                bdFragment = BookDetailsFragment.newInstance(title, author, coverURL);
             } else {
-                bdFragment.setBookDetails(title, author);
+                bdFragment.setBookDetails(title, author, coverURL);
             }
         }
+    }
+
+    // implemented from BookSearchActivity's interface
+    @Override
+    public void onSearch(DialogFragment dialog) {
+        System.out.println("SEARCH");
+    }
+
+    @Override
+    public void onCancel(DialogFragment dialog) {
+        System.out.println("CANCEL");
     }
 
     @Override
@@ -96,5 +124,6 @@ public class MainActivity extends AppCompatActivity implements BookListFragment.
         super.onSaveInstanceState(outState);
         outState.putString(SAVED_TITLE, title);
         outState.putString(SAVED_AUTHOR, author);
+        outState.putString(SAVED_COVER_URL, coverURL);
     }
 }
